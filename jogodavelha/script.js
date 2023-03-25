3c46c9893a77d7598c66d4acf6d6a21870fa5509
Taleszin <santiagold116@gmail.com>
2023-03-24 21:21:31 -0300

    Jogo da velha agora funcional

--- jogodavelha/script.js
+++ jogodavelha/script.js
@@ -7,15 +7,32 @@ let tabuleiro = ["", "", "", "", "", "", "", "", ""];
 const celulas = document.querySelectorAll("td");
 
 // Adicionando um evento de clique em cada célula
-celulas.forEach(celula => celula.addEventListener("click", clicar));
+celulas.forEach((celula, index) => celula.addEventListener("click", () => clicar(index)));
 
 // Função chamada quando uma célula é clicada
-function clicar() {
-    for (let i = 0; i < celulas.length; i++) {
-        celulas[i].addEventListener("click", function() {
-          alert(`vez do jogador ${jogadorAtual}`);
-        });
-      }
+function clicar(index) {
+  if (jogoAcabou || tabuleiro[index] !== "") {
+    return;
+  }
+
+  celulas[index].innerHTML = jogadorAtual;
+  tabuleiro[index] = jogadorAtual;
+
+  jogadorAtual = jogadorAtual === "X" ? "O" : "X";
+
+  if (verificarVitoria()) {
+    setTimeout(() => {
+      alert(`Jogador ${jogadorAtual === "X" ? "O" : "X"} venceu!`);
+      jogoAcabou = true;
+      exibirConfirm();
+    }, 100);
+  } else if (verificarEmpate()) {
+    setTimeout(() => {
+      alert("Deu empate!");
+      jogoAcabou = true;
+      exibirConfirm();
+    }, 100);
+  }
 }
 
 // Função para verificar se há um vencedor
@@ -30,12 +47,14 @@ function verificarVitoria() {
     [0, 4, 8],
     [2, 4, 6]
   ];
+
   for (let i = 0; i < combinacoesVitoria.length; i++) {
     const [a, b, c] = combinacoesVitoria[i];
     if (tabuleiro[a] && tabuleiro[a] === tabuleiro[b] && tabuleiro[a] === tabuleiro[c]) {
       return true;
     }
   }
+
   return false;
 }
 
@@ -43,3 +62,18 @@ function verificarVitoria() {
 function verificarEmpate() {
   return tabuleiro.every(celula => celula !== "");
 }
+
+// Função para exibir o confirm
+function exibirConfirm() {
+  const jogarNovamente = confirm("Deseja jogar novamente?");
+  if (jogarNovamente) {
+    location.reload();
+  } else {
+    alert("OK");
+  }
+}
+
+// Centralizando tabela
+const section = document.querySelector("section");
+const tableWrapper = document.querySelector(".table-wrapper");
+tableWrapper.style.marginTop = `${(section.offsetHeight - tableWrapper.offsetHeight) / 2}px`;
